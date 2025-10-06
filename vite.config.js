7cc8998f049940d4e4814e87304ad3ea13194aaf
Akarsh Gupta <akarshg9669@gmail.com>
2025-10-07 01:58:57 +0530

    inital commit

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ViteImageOptimizer({
-      includePublic: true,
-      png: {
-        quality: 70
-      },
-      jpeg: {
-        quality: 70
-      },
-      jpg: {
-        quality: 70
-      },
-      webp: {
-        quality: 70
-      },
-      avif: {
-        quality: 50
-      },
-      svg: {
-        multipass: true
-      }
-    })
-  ],
+  plugins: [react()],
 })
